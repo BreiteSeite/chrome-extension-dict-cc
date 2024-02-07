e3c7bf203d1e43c319a6e6e094da9db13ac3b38d
Micha K <micha@kuehn.io>
2024-02-07 22:45:36 +0100

    updated to manifest v3

--- contextMenu.js
+++ contextMenu.js
@@ -1,15 +1,15 @@
-chrome.contextMenus.onClicked.addListener(function(info, tab) {
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
     var selection = info.selectionText;
-    
+
     chrome.tabs.create({
         "url": "http://www.dict.cc/?s=" + selection,
         "index": tab.index + 1,
         "openerTabId": tab.id
-   });
-    
+    });
+
 });
 
-chrome.runtime.onInstalled.addListener(function() {
+chrome.runtime.onInstalled.addListener(function () {
     chrome.contextMenus.create({
         "title": chrome.i18n.getMessage("contextMenu", "%s"),
         "contexts": ["selection"],
